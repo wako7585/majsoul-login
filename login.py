953404cd29adb0dc45d748a8a1b776c5d0929853
wako7585 <154062292+wako7585@users.noreply.github.com>
2026-02-17 19:04:34 +0800

    Add: mail reward receive

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+import random
 import sys  
 import os
 from time import sleep, time  
@@ -5,9 +6,64 @@ from datetime import datetime
 from selenium import webdriver  
 from selenium.webdriver import ActionChains  
 from selenium.webdriver.common.by import By  
+from selenium.webdriver.support.ui import WebDriverWait  
+from selenium.webdriver.support import expected_conditions as EC  
   
 log_path = os.path.join(os.getcwd(), "log.txt")    
 action_start_time = time()  
+
+def random_normal_distribution_int(a, b, n=3):
+    """
+    Generate a normal distribution int within the interval.
+    Use the average value of several random numbers to simulate normal distribution.
+
+    Args:
+        a (int): The minimum of the interval.
+        b (int): The maximum of the interval.
+        n (int): The amount of numbers in simulation. Default to 3.
+
+    Returns:
+        int
+    """
+    a = round(a)
+    b = round(b)
+    if a < b:
+        total = 0
+        for _ in range(n):
+            total += random.randint(a, b)
+        return round(total / n)
+    else:
+        return b
+
+def random_rectangle_point(area, n=3):
+    """Choose a random point in an area.
+
+    Args:
+        area: (upper_left_x, upper_left_y, bottom_right_x, bottom_right_y).
+        n (int): The amount of numbers in simulation. Default to 3.
+
+    Returns:
+        tuple(int): (x, y)
+    """
+    x = random_normal_distribution_int(area[0], area[2], n=n)
+    y = random_normal_distribution_int(area[1], area[3], n=n)
+    return x, y
+
+# move_to_element_with_offset 基於中心點偏移 (495, 279)
+def offset_point_from_center(area, origin=(495, 279)): 
+    """ 計算相對於指定原點的偏移座標。 
+
+    Args: 
+    area: (upper_left_x, upper_left_y, bottom_right_x, bottom_right_y).
+    origin (tuple): origin point (0,0) defaults to (495, 279).
+
+    Returns: 
+    tuple(int): (dx, dy) 
+    """ 
+    x, y = random_rectangle_point(area, n=3)
+    dx = x - origin[0] 
+    dy = y - origin[1] 
+    return dx, dy
 
 try:  
     acccounts = int(len(sys.argv[1:]) / 2)  
@@ -28,29 +84,54 @@ try:
 
         # 2. Input email  
         screen = driver.find_element(By.ID, 'layaCanvas')  
+        x, y = offset_point_from_center((640, 160, 830, 185))
         ActionChains(driver) \
-            .move_to_element_with_offset(screen, 250, -100) \
+            .move_to_element_with_offset(screen, x, y) \
             .click() \
             .perform()  
-        driver.find_element(By.NAME, 'input').send_keys(email)  
+        wait = WebDriverWait(driver, 15)
+        screen.screenshot("element1.png")
+        email_input = wait.until(EC.presence_of_element_located((By.NAME, 'input')))
+        email_input.send_keys(email)  
         print('Input email successfully')  
 
         # 3. input password  
+        x, y = offset_point_from_center((640, 220, 830, 245))
         ActionChains(driver) \
-            .move_to_element_with_offset(screen, 250, -50) \
+            .move_to_element_with_offset(screen, x, y) \
             .click() \
             .perform()  
-        driver.find_element(By.NAME, 'input_password').send_keys(passwd)  
+        password_input = wait.until(EC.presence_of_element_located((By.NAME, 'input_password')))
+        password_input.send_keys(passwd)  
         print('Input password successfully')  
 
-        # 4. Click login button  
+        # 4. Click login button 
+        x, y = offset_point_from_center((640, 330, 830, 360))
         ActionChains(driver) \
-            .move_to_element_with_offset(screen, 250, 50) \
+            .move_to_element_with_offset(screen, x, y) \
             .click() \
             .perform()  
         print('Entering game...')  
         sleep(30)
         print('Login success')  
+
+        # 5. Click mail box button
+        x, y = offset_point_from_center((740,30,780,50))
+        ActionChains(driver) \
+            .move_to_element_with_offset(screen, x, y) \
+            .click() \
+            .perform()  
+        print('Goto  mail box')
+        sleep(10)
+
+        # 6. Click receive or delete mail button
+        x, y = offset_point_from_center((565,480,630,505))
+        ActionChains(driver) \
+            .move_to_element_with_offset(screen, x, y) \
+            .click() \
+            .perform()  
+        print('Receive or delete mail')
+        sleep(10)
         driver.quit()  
 
     # no error : login success  
